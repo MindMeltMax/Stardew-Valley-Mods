f6448afb5f4f2d63e954670025ff637dd181a0fd
MindMeltMax <mindmeltmax695@gmail.com>
2022-10-02 23:30:51 +0200

    Added ChangeFarmCaves code

---
+++ Change Farm Caves/ModEntry.cs
@@ -0,0 +1,21 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+
+namespace ChangeFarmCaves
+{
+    internal class ModEntry : Mod
+    {
+        internal static IModHelper IHelper;
+        internal static IMonitor IMonitor;
+        internal static ITranslationHelper ITranslations;
+
+        public override void Entry(IModHelper helper)
+        {
+            IHelper = Helper;
+            IMonitor = Monitor;
+            ITranslations = Helper.Translation;
+
+            Helper.Events.GameLoop.GameLaunched += (s, e) => Patcher.Patch(helper);
+        }
+    }
+}
