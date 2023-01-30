25639011e7d7f75f3a8066152e5be7c4e573f04d
ncarigon <ncarigon@liquidweb.com>
2023-01-30 15:15:10 -0500

    Didn't save all changes on the first commit.

--- MultiPlayer Info/ModEntry.cs
+++ MultiPlayer Info/ModEntry.cs
@@ -2,7 +2,6 @@
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace MPInfo 
@@ -21,6 +20,8 @@ namespace MPInfo
 
             helper.Events.GameLoop.GameLaunched += OnGameLaunched;
 
+            helper.Events.Display.WindowResized += Display_WindowResized;
+
             helper.Events.Multiplayer.PeerConnected += OnPlayerJoin;
             helper.Events.Multiplayer.PeerDisconnected += OnPlayerLeave;
             helper.Events.Multiplayer.ModMessageReceived += OnMultiplayerDataReceived;
@@ -31,19 +32,21 @@ namespace MPInfo
             Patches.Apply(ModManifest.UniqueID);
         }
 
-        private void ResetDisplays() 
+        private void Display_WindowResized(object? sende, WindowResizedEventArgs e)
         {
-            var displays = Game1.onScreenMenus.Where(x => x is PlayerInfoBox).OfType<PlayerInfoBox>().ToArray();
-            var reportedHealthList = new List<int>(displays.Select(x => x.Who.health));
-            for (int i = 0; i < displays.Length; i++)
-                Game1.onScreenMenus.Remove(displays[i]);
-            PlayerInfoBox display = null!;
-            foreach (var player in Game1.getOnlineFarmers()) 
+            if (e.OldSize != e.NewSize)
+                RepositionAll();
+        }
+
+        public static void RepositionAll()
+        {
+            var index = 0;
+            foreach (var pib in Game1.onScreenMenus.Where(x => (x as PlayerInfoBox)?.Visible() ?? false).OfType<PlayerInfoBox>())
             {
-                display = new(player);
-                Game1.onScreenMenus.Add(display);
+                pib.xPositionOnScreen = 32;
+                pib.yPositionOnScreen = (Game1.graphics.GraphicsDevice.Viewport.Height - 32 - 96) - (112 * index);
+                index++;
             }
-            PlayerInfoBox.RedrawAll();
         }
 
         private void OnGameLaunched(object? sender, GameLaunchedEventArgs e) 
@@ -66,7 +69,7 @@ namespace MPInfo
                 setValue: value => 
                 {
                     Config.Enabled = value;
-                    PlayerInfoBox.RedrawAll();
+                    RepositionAll();
                 }
             );
             configMenu.AddBoolOption(
@@ -77,7 +80,7 @@ namespace MPInfo
                 setValue: value => 
                 {
                     Config.ShowSelf = value;
-                    PlayerInfoBox.RedrawAll();
+                    RepositionAll();
                 }
             );
             configMenu.AddBoolOption(
@@ -100,7 +103,9 @@ namespace MPInfo
         {
             lastHealth = Game1.player.health;
             lastMaxHealth = Game1.player.maxHealth;
-            ResetDisplays();
+            foreach (var player in Game1.getOnlineFarmers())
+                Game1.onScreenMenus.Add(new PlayerInfoBox(player));
+            RepositionAll();
         }
 
         private void OnUpdateTicked(object? sender, UpdateTickedEventArgs e) 
@@ -108,31 +113,77 @@ namespace MPInfo
             if (!Context.IsWorldReady)
                 return;
 
-            if (Game1.player.health != lastHealth)
+            if (Game1.player.health != lastHealth) {
+                lastHealth = Game1.player.health;
                 Helper.Multiplayer.SendMessage(Game1.player.health, "MPInfo.Health", new[] { ModManifest.UniqueID });
-            if (Game1.player.maxHealth != lastMaxHealth)
+            }
+            if (Game1.player.maxHealth != lastMaxHealth) {
+                lastMaxHealth = Game1.player.maxHealth;
                 Helper.Multiplayer.SendMessage(Game1.player.maxHealth, "MPInfo.MaxHealth", new[] { ModManifest.UniqueID });
+            }
         }
 
-        private void OnPlayerJoin(object? sender, PeerConnectedEventArgs e) => ResetDisplays();
+        private static void ModifyPlayerBox(long id, bool addPlayer)
+        {
+            var who = Game1.getFarmer(id);
+            Game1.onScreenMenus
+                .Where(x => x is PlayerInfoBox).OfType<PlayerInfoBox>()
+                .Where(x => x.Who.UniqueMultiplayerID == who.UniqueMultiplayerID)
+                .ToList()
+                .ForEach(x => Game1.onScreenMenus.Remove(x));
+            if (addPlayer)
+                Game1.onScreenMenus.Add(new PlayerInfoBox(who));
+            RepositionAll();
+        }
 
-        private void OnPlayerLeave(object? sender, PeerDisconnectedEventArgs e) => ResetDisplays();
+        private void OnPlayerJoin(object? sender, PeerConnectedEventArgs e)
+        {
+            if (Game1.IsMasterGame)
+            {
+                ModifyPlayerBox(e.Peer.PlayerID, true);
+                Helper.Multiplayer.SendMessage(e.Peer.PlayerID, "MPInfo.AddPlayer", new[] { ModManifest.UniqueID });
+            }
+        }
+
+        private void OnPlayerLeave(object? sender, PeerDisconnectedEventArgs e)
+        {
+            if (Game1.IsMasterGame)
+            {
+                ModifyPlayerBox(e.Peer.PlayerID, false);
+                Helper.Multiplayer.SendMessage(e.Peer.PlayerID, "MPInfo.RemovePlayer", new[] { ModManifest.UniqueID });
+            }
+        }
 
         private void OnMultiplayerDataReceived(object? sender, ModMessageReceivedEventArgs e) 
         {
             if (e.FromModID == Helper.ModRegistry.ModID) 
             {
-                if (e.Type == "MPInfo.Health") 
+                var who = Game1.getFarmer(e.FromPlayerID);
+                if (who is not null)
                 {
-                    var display = (PlayerInfoBox?)Game1.onScreenMenus.FirstOrDefault(x => x is PlayerInfoBox pib && pib.Who.UniqueMultiplayerID == e.FromPlayerID);
-                    if (display is not null)
-                        display.Who.health = e.ReadAs<int>();
-                } 
-                else if (e.Type == "MPInfo.MaxHealth") 
-                {
-                    var display = (PlayerInfoBox?)Game1.onScreenMenus.FirstOrDefault(x => x is PlayerInfoBox pib && pib.Who.UniqueMultiplayerID == e.FromPlayerID);
-                    if (display is not null)
-                        display.Who.maxHealth = e.ReadAs<int>();
+                    if (e.Type == "MPInfo.Health")
+                    {
+                        who.health = e.ReadAs<int>();
+                    }
+                    else if (e.Type == "MPInfo.MaxHealth")
+                    {
+                        who.maxHealth = e.ReadAs<int>();
+                    }
+                    else if (e.FromPlayerID == Game1.MasterPlayer.UniqueMultiplayerID)
+                    {
+                        who = Game1.getFarmer(e.ReadAs<long>());
+                        if (who.UniqueMultiplayerID != Game1.player.UniqueMultiplayerID)
+                        {
+                            if (e.Type == "MPInfo.AddPlayer")
+                            {
+                                ModifyPlayerBox(e.ReadAs<long>(), true);
+                            }
+                            else if (e.Type == "MPInfo.RemovePlayer")
+                            {
+                                ModifyPlayerBox(e.ReadAs<long>(), false);
+                            }
+                        }
+                    }
                 }
             }
         }
