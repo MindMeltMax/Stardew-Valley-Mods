5dfa54e107062eb2bc05ac609228d2530466502a
MindMeltMax <49438916+MindMeltMax@users.noreply.github.com>
2023-09-25 09:00:45 +0200

    Update fishnets to 1.0.7

---
+++ Fishnets/External.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Object = StardewValley.Object;
+
+namespace Fishnets
+{
+    public interface IQualityBaitApi
+    {
+        int GetQuality(int currentQuality, int baitQuality);
+    }
+
+    public interface IAlternativeTexturesApi
+    {
+        Texture2D GetTextureForObject(Object obj, out Rectangle sourceRect);
+    }
+
+    public interface IJsonAssetsApi
+    {
+        int GetObjectId(string name);
+    }
+
+    public interface IDynamicGameAssetsApi
+    {
+        string GetDGAItemId(object item);
+        object SpawnDGAItem(string fullId);
+    }
+
+    public interface ISaveAnywhereApi
+    {
+        void addBeforeSaveEvent(string id, Action beforeSave);
+        void addAfterLoadEvent(string id, Action afterLoad);
+        void addAfterSaveEvent(string id, Action afterSave);
+    }
+}
