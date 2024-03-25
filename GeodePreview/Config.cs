1e93aa2b4ec8c07253a09b39ed6c747b21358bc7
MindMeltMax <49438916+MindMeltMax@users.noreply.github.com>
2024-03-25 15:34:19 +0100

    Add Hay Collection and GeodePreview

---
+++ GeodePreview/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeodePreview
+{
+    public class Config
+    {
+        public bool ShowAlways { get; set; } = true;
+
+        public bool ShowStack { get; set; } = true;
+    }
+}
---
+++ Hay Collection/ModEntry.cs
@@ -0,0 +1,17 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HayCollection
+{
+    internal class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.GameLaunched += (_, _) => Patches.Patch(ModManifest.UniqueID);
+        }
+    }
+}
