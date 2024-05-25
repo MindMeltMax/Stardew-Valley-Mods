25e430ad7e146b8015eec9ed2a2267544c7492f1
MindMeltMax <49438916+MindMeltMax@users.noreply.github.com>
2024-05-25 02:01:08 +0200

    Update MPInfo
    Changed data handling by creating a custom object with required health/stamina data and serializing that to player mod data which is read by the mod and shown in the display

---
+++ MultiPlayer Info/PlayerInfo.cs
@@ -0,0 +1,47 @@
+﻿using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPInfo
+{
+    public class PlayerInfo
+    {
+        public int Health { get; set; }
+
+        public int MaxHealth { get; set; }
+
+        public float Stamina { get; set; }
+
+        public int MaxStamina { get; set; }
+
+        public PlayerInfo() { }
+
+        public PlayerInfo(Farmer player)
+        {
+            Health = player.health;
+            MaxHealth = player.maxHealth;
+            Stamina = player.Stamina;
+            MaxStamina = player.MaxStamina;
+        }
+
+        //Using this instead of $"{Health}|{MaxHealth}|{Stamina}|{MaxStamina}"; takes 18 less il instructions... WTF c#?
+        public string Serialize() => new StringBuilder().Append(Health).Append('|').Append(MaxHealth).Append('|').Append(Stamina).Append('|').Append(MaxStamina).ToString();
+
+        public static PlayerInfo Deserialize(string value)
+        {
+            var items = value.Split('|');
+            return new()
+            {
+                Health = int.Parse(items[0]),
+                MaxHealth = int.Parse(items[1]),
+                Stamina = float.Parse(items[2]),
+                MaxStamina = int.Parse(items[3])
+            };
+        }
+
+        public bool IsMatch(Farmer player) => player.health == Health && player.maxHealth == MaxHealth && player.Stamina == Stamina && player.MaxStamina == MaxStamina;
+    }
+}
