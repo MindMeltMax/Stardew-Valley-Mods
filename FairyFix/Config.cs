faf1cf1cf582350ef5e9da0fad4b8cbcc8452e1d
MindMeltMax <49438916+MindMeltMax@users.noreply.github.com>
2024-05-25 16:12:07 +0200

    Upload FairyFix

---
+++ FairyFix/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairyFix
+{
+    public class Config
+    {
+        public SelectionMode SelectMode { get; set; } = SelectionMode.ConnectedSameCrop;
+
+        public bool ReviveDeadCrops { get; set; } = true;
+
+        public bool ResetOnSeasonChange { get; set; } = true;
+    }
+}
