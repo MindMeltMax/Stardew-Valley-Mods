cf4b683bc35fbca7a5db8acbd0ac36646410c3a9
MindMeltMax <49438916+MindMeltMax@users.noreply.github.com>
2024-06-15 19:15:09 +0200

    Updates
    * [Change Farm Caves] Update manifest for new version with french translations (thanks Caranud)
    + [Glow Buff] Add glow buff initial version

---
+++ Glow Buff/LightSourceData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlowBuff
+{
+    public record LightSourceData
+    {
+        public int TextureId { get; set; }
+
+        public int Radius { get; set; }
+
+        public Color Color { get; set; }
+
+        public int Duration { get; set; }
+
+        public bool PrismaticColor { get; set; }
+
+        public string Source { get; set; }
+    }
+}
